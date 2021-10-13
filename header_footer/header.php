4a882a1b2e6b294e8285f0069cb91f7556b2ef2a
ferbaeza <baezeta@gmail.com>
2021-10-13 17:18:23 +0200

    header1

---
+++ header_footer/header.php
@@ -0,0 +1,16 @@
+<?php
+    echo ' <div class="top_nav">
+        <nav class="nav_uno">
+            <ul>
+                <li class="bot_nav"><a href="">Home</a></li>
+                <li class="bot_nav"><a href="">Nes</a></li>
+                <li class="bot_nav"><a href="">SNes</a></li>
+                <li class="bot_nav"><a href="">Megadrive</a></li>
+                <li class="bot_nav"><a href="">Capcom</a></li>
+                <li class="bot_nav" style="float:right"><a href="Carrito"></a>Carrito<span>🛒</span></li>
+            </ul>
+                
+        </nav>
+    </div>';
+
+?>
