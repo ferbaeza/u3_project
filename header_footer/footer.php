bef5561a99424b4d79edf1b721a434800348abfa
Henry <henryjeje@hotmail.com>
2021-10-14 18:59:06 +0200

    footer añadido

---
+++ header_footer/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+echo '<footer class="footer_case">
+        <nav id="nav_footer">
+            <ul id="ul_footer">
+                <li id="li_footer"><a href="index.php"><img id="footer_ico"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"/></a></li>
+                <li><a href="politica_privacidad.php"><img id="footer_ico"  src="https://storage.googleapis.com/support-kms-prod/ZAl1gIwyUsvfwxoW9ns47iJFioHXODBbIkrK"/></a></li>
+                <li><a href="politica_privacidad.php"><img id="footer_ico"  src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png"/></a></li>
+                <li><a href="preguntas_frecuentes.php"><img id="footer_ico"  src="https://logodownload.org/wp-content/uploads/2017/11/discord-logo-4-1.png"/></a></li>
+                <li><a href="login.php"><img id="footer_ico"  src="https://www.pngrepo.com/png/113433/512/logout.png"/></a></li>
+            </ul>
+        </nav>
+    </footer>';
+
+?>
