1bd6e987475e08ff38e36ec332a767fd35f036e7
ferbaeza <baezeta@gmail.com>
2021-10-15 19:49:06 +0200

    viernes

--- header_footer/header.php
+++ header_footer/header.php
@@ -2,10 +2,11 @@
     echo ' <div class="top_nav">
         <nav class="nav_uno">
             <ul>
-                <li class="bot_nav"><a href="">Home</a></li>
+                <li class="bot_nav"><a href="../index.php">Home</a></li>
                 <li class="bot_nav"><a href="">Nes</a></li>
                 <li class="bot_nav"><a href="">SNes</a></li>
                 <li class="bot_nav"><a href="">Megadrive</a></li>
+                <li class="bot_nav"><a href="">MasterSystem</a></li>
                 <li class="carrito"><a href="Carrito">Carrito 🛒</a></li>
             </ul>
                 
