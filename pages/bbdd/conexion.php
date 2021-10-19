c434fb1a1798dc9a7005cfdfbd99745efc39138a
ferbaeza <baezeta@gmail.com>
2021-10-19 16:09:24 +0200

    bd

--- pages/bbdd/conexion.php
+++ pages/bbdd/conexion.php
@@ -2,7 +2,7 @@
     $server = "localhost";
     $user = "root";
     $password = "";
-    $database = "u3"; //Hay que modificar el nombre de la base de datos a la que vayamos a consultar
+    $database = "tiendaretro"; //Hay que modificar el nombre de la base de datos a la que vayamos a consultar
 
 
     //conexion en bbdd
