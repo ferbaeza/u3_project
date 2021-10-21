8e0f7ebabd25a37393ed4cb769254ed335eca33d
Henry <henryjeje@hotmail.com>
2021-10-21 18:28:59 +0200

    añadido bd en .sql

---
+++ pages/policy/policies and privacy.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php require_once "header.php"?>
+    <div class="main_dos">
+        <h3 id='titu'>Lorem Ipsum Polies and Privaty</h3>
+        <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."<br>
+            "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor."</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget ipsum eget ante fermentum mollis at at tortor. Aenean non lacus augue. Maecenas vel ipsum ornare, vestibulum ipsum quis, luctus eros. Mauris nec convallis metus, vitae porta ex. Pellentesque consectetur sodales neque a rhoncus. Aliquam id tortor nec purus interdum blandit. Integer non mauris nec eros tempor rutrum vel sed orci. Nulla consectetur venenatis est, in elementum justo molestie ac. Proin egestas pellentesque libero, sit amet pharetra diam. Aliquam gravida eros neque, non finibus urna laoreet in. Praesent tempor fringilla nisl sed scelerisque. Nullam a vestibulum augue. Duis rutrum lorem nunc, ut eleifend nibh fermentum venenatis. Curabitur elit quam, mattis vel leo et, sollicitudin facilisis dolor. Integer quis orci ac velit porttitor iaculis. Sed nec mattis enim.</p>
+
+        <p>Nullam commodo quis felis vel luctus. Morbi ornare urna ac elementum egestas. Phasellus eu eros eget justo luctus vulputate. Nunc pharetra, ligula ut cursus eleifend, lacus erat gravida libero, tristique egestas quam urna vel dolor. Sed pretium, dolor id sollicitudin imperdiet, tortor massa pretium leo, a facilisis nunc nisl eu diam. Quisque arcu quam, maximus quis diam eu, egestas egestas felis. In hac habitasse platea dictumst.</p>
+
+        <p>Aliquam at sagittis felis. Vestibulum non tincidunt lorem. Donec eget ipsum dapibus risus sollicitudin cursus. Sed tempus magna elit, feugiat placerat arcu semper vitae. Phasellus nec diam vel erat commodo fringilla. Proin nec nisi dui. Morbi efficitur cursus sapien, nec fermentum justo lobortis id. Aliquam auctor suscipit sagittis. Duis eget libero auctor, gravida nisi sit amet, accumsan ligula. Ut vitae libero dolor. Etiam vitae egestas turpis. Aliquam id tempor odio. Nullam vel bibendum ante. Mauris placerat molestie commodo. Praesent pulvinar felis eget dui porta suscipit.</p>
+
+        <p>Proin feugiat gravida semper. Donec tortor lacus, aliquet sit amet orci vel, condimentum lacinia arcu. Sed ac porttitor sem, consectetur ullamcorper libero. Phasellus sed tincidunt arcu, id aliquet massa. Vestibulum id consectetur nibh. Cras in sodales nunc, non viverra sapien. Duis id iaculis neque. Donec accumsan efficitur purus, tempor consectetur lacus malesuada sed. Nunc pellentesque et nisl ut laoreet. In ex odio, elementum at sagittis quis, dapibus id lacus. Ut maximus nunc consequat, malesuada elit sed, tempus quam. Phasellus volutpat sed nibh consectetur egestas.</p>
+
+        <p>Etiam porta massa in rhoncus luctus. Phasellus efficitur eget eros molestie blandit. Ut vitae molestie odio, dapibus porttitor ipsum. Pellentesque semper, nulla sit amet convallis auctor, diam nisl volutpat sem, quis tempus tellus odio id massa. Suspendisse rutrum, eros vitae lobortis lacinia, neque elit pellentesque libero, non tempor dui odio vel est. Phasellus sagittis aliquam efficitur. Morbi vel nulla vitae elit eleifend mattis at vitae magna.</p>
+
+
+    </div>
+</body>
+</html>
+
+
