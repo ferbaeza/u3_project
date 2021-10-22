7ab719a0a7358af457b16764044f01ec63669297
ferbaeza <fbaezahurtado@gmail.com>
2021-10-22 19:54:58 +0200

    21_oct

--- pages/consolas/nes.php
+++ pages/consolas/nes.php
@@ -16,16 +16,31 @@
 <?php
     require_once "../bbdd/conexion.php";
 
-/*
+
     $con_nes = $bd->prepare("SELECT * FROM game WHERE game.id_console=1; ");
     $con_nes->execute();
+    //$results = $con_nes->fetchAll();
+
+    echo '<div class="nes">';
+    echo '<h2>Videojuegos disponibles</h2>';
+
+    foreach($con_nes as $row){
+        echo "<div id='game' class=".$row['id_game'].">" ;
+        echo $row['name']."". $row['year']."<button id=".$row['id_game']." name=''>Anadir</button><br>";
+        
+        
+        echo "</div>";
+
+
+
+        
+    }
+
     $results = $con_nes->fetchAll(PDO::FETCH_ASSOC);
-    $json_nes = json_encode($results);
-    $j_nes = json_decode($json_nes);
-    while ($row = $j_nesj->fetch()) {
-        print_r($row);
-    }    
-*/
+
+
+
+
 ?>
 
 
