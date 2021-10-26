46c99095d7317216d62f8a8ae7db0f045746d9cb
FerBaeza <fbaezahurtado@gmail.com>
2021-10-26 21:34:20 +0200

    26Oct

--- pages/js/loadConsoles.js
+++ pages/js/loadConsoles.js
@@ -1,6 +1,6 @@
 function loadConsole(console, consolas) {
     let gameConsole='<div>';
-    gameConsole+='<div>';
+    gameConsole+='<div class="conquery">';
     gameConsole+='<h2>'+console.name+'</h2>';
     gameConsole+='<div><a href="games.php?id='+console.id_console+'"<div><img src='+console.imagen+'></div></div>';
     gameConsole+='</div></a>';
