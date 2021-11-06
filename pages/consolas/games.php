78cf8bda78bbd0f2c5d44f005b781768b4dde7c0
Henry <henryjeje@hotmail.com>
2021-11-06 10:29:35 +0100

    actualizacion carrito

--- pages/consolas/games.php
+++ pages/consolas/games.php
@@ -6,7 +6,6 @@
     <link href="../../css/header.css" rel="stylesheet" type="text/css">
     <link href="../../css/consolas.css" rel="stylesheet" type="text/css">
     <script type="text/javascript" src="../js/loadGames.js"></script>
-    <script type="text/javascript" src="../shop_cart/shop_cart.js"></script>
 </head>
 <body>
     <?php include_once "../../pages/header_footer/main_header.php"?>
